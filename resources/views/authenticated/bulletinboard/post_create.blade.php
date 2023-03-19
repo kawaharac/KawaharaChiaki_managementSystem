b067102759521f2947be95d52d2f2942fe98e398
USER\USER <kwhr5cak31@gmail.com>
2023-03-19 18:05:26 +0900

    サブカテゴリー表示（中身修正はまだ）

--- resources/views/authenticated/bulletinboard/post_create.blade.php
+++ resources/views/authenticated/bulletinboard/post_create.blade.php
@@ -36,13 +36,19 @@
   @can('admin')
   <div class="w-25 ml-auto mr-auto">
     <div class="category_area mt-5 p-5">
+      <!-- カテゴリー選択#773 -->
       <div class="">
         <p class="m-0">メインカテゴリー</p>
         <input type="text" class="w-100" name="main_category_name" form="mainCategoryRequest">
         <input type="submit" value="追加" class="w-100 btn btn-primary p-0" form="mainCategoryRequest">
       </div>
-      <!-- サブカテゴリー追加 -->
-      <form action="{{ route('main.category.create') }}" method="post" id="mainCategoryRequest">{{ csrf_field() }}</form>
+      <!-- サブカテゴリー追加(修正：mainからsubに)  -->
+      <div class="">
+        <p class="m-0">サブカテゴリー</p>
+        <input type="text" class="w-100" name="sub_category_name" form="subCategoryRequest">
+        <input type="submit" value="追加" class="w-100 btn btn-primary p-0" form="subCategoryRequest">
+      </div>
+      <form action="{{ route('sub.category.create') }}" method="post" id="subCategoryRequest">{{ csrf_field() }}</form>
     </div>
   </div>
   @endcan
