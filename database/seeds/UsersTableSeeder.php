ab850989daf84b4437f923129835a578af35935f
USER\USER <kwhr5cak31@gmail.com>
2023-04-29 18:47:39 +0900

    seedでデータ追加

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,13 +18,25 @@ class UsersTableSeeder extends Seeder
                 'under_name' => 'man',
                 'over_name_kana' => 'アンパン',
                 'under_name_kana' => 'マン',
-                'mail_address' => 'aaa@com',
+                'mail_address' => 'ddd@com',
                 'sex' => '1', //性別(1が男)
                 'birth_day' => '2000-01-01', //誕生日
                 'role' => '1', //権限
                 'password' => bcrypt('123') //ハッシュ化する>bcrypt('')
                 //'remember_token' => ''
                 //リメンバートークンはNULLでオッケーと書いてあったのでコメントアウトして様子見
+            ],
+            [
+                'over_name' => 'baikin',
+                'under_name' => 'man',
+                'over_name_kana' => 'バイキン',
+                'under_name_kana' => 'マン',
+                'mail_address' => 'ccc@com',
+                'sex' => '1', //性別(1が男)
+                'birth_day' => '2000-10-11', //誕生日
+                'role' => '1', //権限
+                'password' => bcrypt('456')
+                //追加4/29
             ]
         ]);
 
